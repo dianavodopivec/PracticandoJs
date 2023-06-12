fbe8257688dce2da1385e28354b4c6a3dfef7781
dianavodopivec <dianavodopivec16@gmail.com>
2023-06-11 22:51:05 -0300

    sumasjs

---
+++ practica1.js
@@ -0,0 +1,9 @@
+function sumarNumeros(numero1, numero2) {
+  let resultado = numero1 + numero2;
+  return resultado;
+}
+
+console.log(sumarNumeros(1, 2))
+console.log(sumarNumeros(2, 3))
+console.log(sumarNumeros(3, 4))
+console.log(sumarNumeros(4, 5))
