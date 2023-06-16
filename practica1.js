af755452ada4c4d451177732f3c887d3d4074fe0
dianavodopivec <dianavodopivec16@gmail.com>
2023-06-16 01:46:11 -0300

    task complete

--- practica1.js
+++ practica1.js
@@ -1,34 +1,28 @@
 
-function sumarNumeros(numero1, numero2) {
+function sumaNumeros(numero1, numero2) {
     let resultado = numero1 + numero2;
     return resultado;
-  }
-  
-  console.log(sumarNumeros(1, 2))
-  console.log(sumarNumeros(2, 3))
-  console.log(sumarNumeros(3, 4))
-  console.log(sumarNumeros(4, 5))
-
-function sumarNumeros(numero1, numero2, numero3, numero4, fruta1, fruta2) {
-    let resultado1 = numero1 + numero2;
-    let resultado2 = numero3 - numero4;
-    let canasta = fruta1 + fruta2;
-    return { resultado1, resultado2, canasta };
 }
 
-let miResultado1 = sumarNumeros(20, 2, 8, 12, "frutilla", "banana").canasta;
-console.log(miResultado1)
+console.log(sumaNumeros(2, 3))
+console.log(sumaNumeros(5, 5))
 
+function sumaNumeros2(numb1, numb2, numb3, numb4) {
+    let resultadoSuma = numb1 + numb2;
+    let resultadoResta = numb3 - numb4; 
+    return {resultadoSuma, resultadoResta};
+}
 
-/*
-let humano = {
-    nombre: ["Diana", "Matias"],
-    edad: 22,
-};
+let resultadoNumeros2 = sumaNumeros2(2, 3, 5, 5);
+console.log(resultadoNumeros2)
 
-let miNombre = humano.nombre[1]
-console.log(miNombre)
-
-/*Propiedades de un objeto se accede con un . y a las posiciones de un array con [] y número de la posición 
-Sumar números retorna objetos
-*/
+function sumaNumeros2(numb1, numb2, numb3, numb4) {
+    let resultadoSuma = numb1 + numb2;
+    let resultadoResta = numb3 - numb4;
+    return {resultadoSuma, resultadoResta};
+  }
+  
+  let {resultadoSuma, resultadoResta} = sumaNumeros2(2, 3, 5, 5);
+  console.log(resultadoSuma);
+  console.log(resultadoResta);
+  
