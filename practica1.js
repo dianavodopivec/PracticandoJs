a0cdcd0d6ae8dc24a66cf7ee448d463264aa0142
dianavodopivec <dianavodopivec16@gmail.com>
2023-06-26 08:16:31 -0300

    clase 22

--- practica1.js
+++ practica1.js
@@ -1,4 +1,4 @@
-
+/*
 function sumaNumeros(numero1, numero2) {
     let resultado = numero1 + numero2;
     return resultado;
@@ -13,7 +13,7 @@ function sumaNumeros2(numb1, numb2, numb3, numb4) {
     return {resultadoSuma, resultadoResta};
 }
 
-let resultadoNumeros2 = sumaNumeros2(2, 3, 5, 5);
+let resultadoNumeros2 = sumaNumeros2(2, 3, 5, 5).resultadoResta;
 console.log(resultadoNumeros2)
 
 function sumaNumeros2(numb1, numb2, numb3, numb4) {
@@ -25,4 +25,150 @@ function sumaNumeros2(numb1, numb2, numb3, numb4) {
   let {resultadoSuma, resultadoResta} = sumaNumeros2(2, 3, 5, 5);
   console.log(resultadoSuma);
   console.log(resultadoResta);
-  
+*/
+/*
+const diana = {
+    nombre: "Diana",
+    apellido: "Vodopivec",
+    edad: 22,
+    pasatiempos: ["Estudiar", "Jugar juegos", "Cocinar postrecitos"],
+    comidasfavoritas: ["Pizza", "Sushi", "Fideos", "Arroz"],
+    soltero: true, 
+    description: function () {
+        console.log(this.comidasfavoritas);
+    },
+};
+
+diana.description()
+
+console.log(diana);
+console.log(diana["nombre"]);
+console.log(diana["apellido"]);
+console.log(diana["edad"]);
+*/
+
+/*
+let a = 5 + (5 - 10) * 3;
+let modulo = 5 % 2;
+
+console.log(a);
+console.log(modulo);
+console.log(8 > 9);
+console.log(8 < 9);
+console.log(8 >= 9);
+console.log(8 <= 9);
+console.log(7 == 7);
+console.log("7" == 7);
+console.log(0 == false);
+console.log(0 === false)
+
+/*let i = 1;
+
+i = i + 2;
+i += 3;
+i -= 3;
+i /= 3;
+i *= 3;
+console.log(i);
+i ++;
+i --;
+++ i;
+-- i;
+console.log(i);*/
+/*
+console.log(!true);
+console.log(4 > 5 || 7 > 1);
+console.log(4 > 5 && 7 > 1);
+console.log(9 === "9") || ("9" === 9);
+console.log(9 === 9) && ("9" === "9");
+
+
+
+                                          //CLASE 22 - PROTOTIPOS//
+/*
+POO (Programación Orientada a Objetos) => 
+Conceptos: 
+ Clases: Modelo a seguir.
+ Objetos: Instancia de una clase.
+  Atributos: Es una característica o propiedad del objeto (son variables dentro de un objeto). 
+  Métodos: Acciones que un objeto puede realizar (funciones dentro de un objeto).
+
+Prototipo en JavaScript: "Mecanismo por el cual un objeto puede heredar de un objeto padre, atributos y métodos"
+ */
+
+//OBJETOS LITERALES y su PROTOTIPO es OBJECT//
+/*
+const animal = {
+  nombre: "vex",
+  sonar() {
+    console.log("Hago soniditos de animalitos");
+  },
+};
+
+const animal2 = {
+  nombre: "cocho",
+  sonar() {
+    console.log("Hago soniditos de animalitos");
+  },
+};
+
+console.log(animal);
+console.log(animal2);
+
+//Función constructora: Se construye una sola vez y a partir de ella, se generan nuevos objetos o instancias de este tipo de función; Construye un prototipo vacío; Los atributos y métodos de este tipo de funciones tienen que "colgar" del this/
+
+/*function Animal1 (nombre, genero, tipo) {
+  //ATRIBUTOS
+  this.nombre = nombre;
+  this.genero = genero;
+  this.tipo = tipo;
+
+  //MÉTODOS
+  this.sonar = function () {
+    console.log("Hago soniditos de animalitos");
+  }
+  this.saludar = function () {
+    console.log(`Hola, me llamo ${this.nombre} y soy muy sucio`);
+
+  }
+}
+
+//NEW -> Nos permite crear un nuevo objeto del tipo que especificamos.
+
+const vex = new Animal1("Vex", "Hembra","Perro"), 
+cocho = new Animal1("Cocho", "Macho", "Gato");
+
+console.log (vex);
+console.log (cocho);
+
+vex.saludar(); 
+cocho.saludar(); */
+
+//SEPARAMOS LOS MÉTODOS DE LA FUNCIÓN PARA ASIGNAR LOS PROTOTIPOS//
+
+function Animal1 (nombre, genero, tipo) {
+  //ATRIBUTOS
+  this.nombre = nombre;
+  this.genero = genero;
+  this.tipo = tipo;
+}
+
+//MÉTODOS AGREGADOS AL PROTOTIPO DE LA FUNCIÓN CONSTRUCTURA, ASIGNANDO POR FUERA AL PROTOTIPO, EVITANDO QUE LAS INSTANCIAS DUPLIQUEN LOS MÉTODOS INTERNAMENTE, MEJORANDO ASÍ EL RENDIMIENDO Y ESPACIO MEMORIA//
+
+  Animal1.prototype.sonar = function () {
+    console.log("Hago soniditos de animalitos");
+  }
+  Animal1.prototype.saludar = function () {
+    console.log(`Hola, me llamo ${this.nombre} y soy muy sucio`);
+
+  }
+
+const vex = new Animal1("Vex", "Hembra","Perro"), 
+cocho = new Animal1("Cocho", "Macho", "Gato");
+
+console.log (vex);
+console.log (cocho);
+
+vex.sonar(); 
+cocho.saludar(); 
+
