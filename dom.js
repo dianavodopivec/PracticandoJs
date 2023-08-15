be2aa99dbb669a2e4ddf9ff56a67ffde2794cb15
dianavodopivec <dianavodopivec16@gmail.com>
2023-08-15 08:03:07 -0300

    Clase 61 - DOM

--- dom.js
+++ dom.js
@@ -1,1 +1,11 @@
+let texto = "SALVADO"
+
+const hablar = (texto) => speechSynthesis.
+speak(new SpeechSynthesisUtterance(texto))
+
+hablar(texto)
+
 //-------------------------------------- Clase 61 - DOM (Introducción) --------------------------------------//
+
+console.log("ELEMENTOS DEL DOCUMENTO")
+console.log(window.document)
