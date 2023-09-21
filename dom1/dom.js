1768bf4b95af9f541cb729773333b4c0c3c8b1ed
dianavodopivec <dianavodopivec16@gmail.com>
2023-09-21 08:39:46 -0300

    Clase 75 - StopPropagation & preventDefault

--- dom1/dom.js
+++ dom1/dom.js
@@ -369,22 +369,3 @@ $eventomultiple.addEventListener("click", () => {
   $eventoRemover.disabled = true //Aplicar la propiedad disabled al true para que desaparezca el botón
 })*/
 
-//-------------------------------------- Clase 74 - Flujo de Eventos (Burbuja y captura) --------------------------------------//
-
-//UNA VEZ QUE UN EVENTO SE ORIGINA, TIENE UNA PROPAGACIÓN A LO LARGO DEL ÁRBOL DEL DOM, ES POR ESO MISMO QUE ESA PROPAGACIÓN SE DA DEL ELEMENTO MÁS INTERNO AL MÁS EXTERNO (FASE DE BURBUJA)
-//DOS MANERAS DE PROPAGAR UN EVENTO
-
-const $divsEventos = document.querySelectorAll(".eventos-flujo div")
-console.log($divsEventos)
-
-
-/*function flujoEventos(e){
-  console.log(`Holi`)
-}
-
-
-$divsEventos.forEach((div) => {
-  div.addEventListener("click", flujoEventos)
-})*/
-
-//-------------------------------------- Clase 75 - StopPropagation & preventDefault --------------------------------------//
